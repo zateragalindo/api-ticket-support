c8d9c5db07777df41f75936a064363b00ad06dbf
Gabriel Galindo <zatera_@hotmail.com>
2024-10-25 22:42:27 -0300

    Adding ticket variable in create.js file

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
-  return response.end("Teste ok !")
+  const { equipment, description, user_name } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+
+  return response.end(JSON.stringify(ticket))
 }
